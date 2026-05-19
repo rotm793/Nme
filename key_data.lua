d21b1c8a939377691afb65a1520f6d7e79170b90
rotm793 <rotm793@gmail.com>
2026-05-19 20:32:25 +0300

    Delete key_data.lua

--- key_data.lua
+++
@@ -1,9 +0,0 @@
-local KeySistemi = {
-    -- ["Oluşturduğun_Şifre"] = Yetki_Durumu (true = şifre üretebilir, false = sadece scripti kullanabilir)
-    
-    ["flopsu12"] = true,       -- Bu senin ana (Owner) şifren. Şifre üretme yetkisi var.
-    ["misafir34"] = false,     -- Bu sadece scripti kullanabilir, şifre üretemez.
-    ["kanka77"] = true,        -- Bu arkadaşın hem scripti kullanır hem de yeni şifre üretebilir.
-}
-
-return KeySistemi
