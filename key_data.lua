5498507cc4bb171938c05e712cbb5ae08d505571
rotm793 <rotm793@gmail.com>
2026-05-19 20:35:36 +0300

    Create key_data.lua

---
+++ key_data.lua
@@ -0,0 +1,6 @@
+local KeySistemi = {
+    ["flopsu12"] = true,       -- Bu senin ana şifren. Hem scripti açar hem yeni şifre üretir.
+    ["swturkboklari"] = false, -- Bu yeni şifre. Sadece scripti açar, şifre üretme yetkisi YOKTUR.
+}
+
+return KeySistemi
