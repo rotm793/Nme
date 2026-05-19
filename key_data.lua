e1b1d6c73a9978c1b030aecb8df5b5c8e3c79072
rotm793 <rotm793@gmail.com>
2026-05-19 20:42:48 +0300

    Remove unnecessary comments from key_data.lua

--- key_data.lua
+++ key_data.lua
@@ -1,7 +1,6 @@
 local KeySistemi = {
-    ["flopsu12"] = true,       -- Bu senin ana şifren. Hem scripti açar hem yeni şifre üretir.
-    ["swturkboklari"] = false, -- Bu yeni şifre. Sadece scripti açar, şifre üretme yetkisi YOKTUR.
-    ["SW_1xEMGidg"] = false,
+    ["flopsu12"] = true,
+    ["swturkboklari"] = false,
 }
 
 return KeySistemi
