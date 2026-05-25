cdb2c128cfe3271df7e3e421a13a1449e7e78280
rotm793 <rotm793@gmail.com>
2026-05-25 20:25:36 +0300

    Update and rename KeyList.lua to normal_keys.lua

--- KeyList.lua
+++
@@ -1,9 +0,0 @@
-local NormalSifreler = {
-    ["swhub10"] = true,
-    ["egesw"] = true,
-    ["umutsw"] = true,
-    ["astasw"] = true,
-    ["esperussw"] = true,
-    ["melihsw"] = true,
-}
-return NormalSifreler
